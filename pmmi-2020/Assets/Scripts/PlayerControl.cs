7680cb0ec5dc596262924a221f5f42e0ef2472b8
Alexander <54832407+PilzAlexander@users.noreply.github.com>
2020-11-20 13:32:28 +0100

    Praktikumsaufgabe_1

---
+++ pmmi-2020/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+    public bool keyboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (keyboard == true)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal * speed, 0f, moveVertical * speed);
+
+
+
+            rb.AddForce(movement);
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
+}
