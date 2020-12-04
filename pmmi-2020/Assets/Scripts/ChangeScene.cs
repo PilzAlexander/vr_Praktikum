44a7edb813933559afe094ca5cb22156f94d13f5
alp1656 <alp1656@thin.onmicrosoft.com>
2020-12-04 16:16:25 +0100

    Erste Sicherung 2. Praktikum

---
+++ pmmi-2020/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public string scene;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(this.transform.eulerAngles.y >= 50)
+        {
+            Debug.Log("50 Grad überschritten");
+            SceneManager.LoadScene(1);
+        }
+    }
+}
